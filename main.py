c0def6ccda6c2e770d42257aaed48d49b3eae448
Sourcery AI <>
2022-08-02 18:48:50 +0000

    'Refactored by Sourcery'

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ def refresh_token(event, context):
     secret_client = secretmanager_v1.SecretManagerServiceClient()
     project_id = os.environ['GCP_PROJECT']
     secret_parent = f'projects/{project_id}'
-    secret_name = secret_parent + f'/secrets/{secret_id}'
-    secret_version = secret_name + '/versions/latest'
+    secret_name = f'{secret_parent}/secrets/{secret_id}'
+    secret_version = f'{secret_name}/versions/latest'
     # fetch secret with current token
     secret_data = secret_client.access_secret_version(request={'name': secret_version})
     #print("DEBUG: ", secret_data)
     # decode and format
     payload = secret_data.payload.data.decode('UTF-8')
-    current_token = 'Bearer ' + str(payload)
+    current_token = f'Bearer {str(payload)}'
     headers = {'authorization': current_token}
     # request to refresh the token
     refresh_token = requests.get(console_address, headers=headers, verify=False)
